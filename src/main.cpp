fe2fbea9eddee8928678771edff5f8ce52c3e0bb
Eduardo Nola <eduardo@bitpagos.co>
2020-07-02 19:58:18 -0300

    Old fine tunning

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 // Define Joystick Connections
 #define JoyStick_X_PIN A1
 #define JoyStick_Y_PIN A0
+#define JoyStick_B_PIN 3
 
 // Declare unsigned 8-bit joystick array
 int joystick[3];
@@ -33,27 +34,32 @@ byte slaceAdd[5] = {'R', 'x', 'A', 'A', 'A'};
 void setup() {
   initialGeneric();
   DEB_DO_PRINTLN(MSG_START);
+
+  pinMode(JoyStick_B_PIN, INPUT_PULLUP);
+
   // Interface RF24
-  Serial.println("Radio Begin");
+  DEB_DO_PRINTLN("Radio Begin");
   radio.begin();
   radio.setDataRate(RF24_250KBPS);
   radio.enableAckPayload();
   radio.setRetries(3, 5);
   radio.openWritingPipe(slaceAdd);
-  Serial.println("Radio Conf");
+  DEB_DO_PRINTLN("Radio Conf");
 }
 
 void loop() {
   // Read Joystick values and map to values of 0 - 255
   joystick[0] = map(analogRead(JoyStick_X_PIN), 0, 1023, 0, 255);
   joystick[1] = map(analogRead(JoyStick_Y_PIN), 0, 1023, 0, 255);
-  joystick[2] = 100;
+  joystick[2] = digitalRead(JoyStick_B_PIN) == LOW ? 1 : 0; // LOW = Se apreto
 
   data.action = 'a';
   data.data[0] = joystick[0];
   data.data[1] = joystick[1];
-  Serial.println(data.data[0]);
-  Serial.println(data.data[1]);
+  data.data[2] = joystick[2];
+  DEB_DO_PRINTLN(data.data[0]);
+  DEB_DO_PRINTLN(data.data[1]);
+  DEB_DO_PRINTLN(data.data[2]);
 
   bool rta = radio.write(&data, sizeof(data));
   if (rta) {
@@ -61,10 +67,10 @@ void loop() {
       int ack;
       radio.read(&ack, sizeof(ack));
     } else {
-      Serial.println("Fail Ack");
+      DEB_DO_PRINTLN("Fail Ack");
     }
   } else {
-    Serial.println("Tx Failed");
+    DEB_DO_PRINTLN("Tx Failed");
   }
 
   delay(100);
